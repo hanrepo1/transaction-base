f269793d5905d6647a37b5e5296350f2f7951c42
hanrepo <hanre.polisar@yahoo.com>
2025-01-30 20:31:13 +0700

    first commit

---
+++ src/main/java/com/example/transaction_base/TransactionBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transaction_base/TransactionBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transaction_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
